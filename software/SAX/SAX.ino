51e74eab38cb5500132730a81d56535a5e74e098
tomcombriat <tomcombriat@live.fr>
2020-05-19 21:55:45 +0200

    Fixed chords, broken by previous commits

--- software/SAX/SAX.ino
+++ software/SAX/SAX.ino
@@ -183,7 +183,7 @@ void setup() {
   display.setCursor(35, 50);
 
   for (int i = 0; i < 3; i++) arp[i].set_notes(arp_N[i], arp_times[i], arp_notes[i], arp_name[i], arp_long_names[i]);
-  for (int i = 0; i < 3; i++) chords[i].set_notes(chord_N[i], chord_notes[i], chord_name[i]);
+  for (int i = 0; i < 3; i++) chords[i].set_notes(chord_N[i], chord_notes[i], chord_name[i], chord_long_names[i]);
 
 
 
